6b1f68a7d49b4fb505497cfe506f1f4a2a356610
glalonde <glalonde@mit.edu>
2013-04-12 19:09:33 -0400

    added some stuff for chassis and robot definitions

--- chassis/chassis.py
+++ chassis/chassis.py
@@ -1,25 +1,10 @@
 #everything is relative to 'center of mass'
+import numpy as np
 
 class chassis():
-  coxaLen = [0]*6
-  coxaMaxAngle = [0]*6
-  coxaMinAngle = [0]*6
-  coxaRadius = [0]*6
-
-  femurLen = [0]*6
-  femurMaxAngle = [0]*6
-  femurMinAngle = [0]*6
-  femurRadius = [0]*6
-
-  tibiaLen = [0]*6
-  tibiaMaxAngle = [0]*6
-  tibiaMinAngle = [0]*6
-  tibiaRadius = [0]*6
-
-
-  #   0 1
-  #  5   2
-  #   4 3
+  #   5 0
+  #  4   1
+  #   3 2
 
   #1 leg specified => radially symmetric
   #2 legs specified => front-back/side symmetric
@@ -28,7 +13,43 @@ class chassis():
   #5 legs specified => wtf man
   #6 legs specified = no symmetry
 
-  def __init__(robotfile):
+
+
+  '''
+  The numpy arrays below store the structural data for the robot
+  For the first array from center to coxa, this is simply a 3D vector
+  For the ones after that, it is a 2D vector (representing cylindrical coordinates from one end of the limb to the other)
+  However, these include 3 more pieces of data: Max angle, Min angle, Radius
+  The angles are the limits on the rotation for that joint, and the radius is the 'radius' of that limb
+  If I feel like implementing some preemptive collision detection
+  So the first one looks like [X,Y,Z]X6
+  The next 3 look like [X,Y,MaxAngle,MinAngle,Radius]
+
+  '''
+
+  #Chassis shape: Chassis -> Coxa vector is the only 3D one, with Z out of the top of the chassis, X out the front, and Y out of the right
+  #All of the other vectors are just 2D, with the angle of the servo making up the third dimension(Cylindrical coordinates)
+  centerToCoxaVector = np.array([[0]*3]*6) #Vector from center to pivot point of coxa
+  coxaToFemurVector = np.array([[0]*5]*6)   #Vector from terminus of the previous coxa vector to the pivot point for the coxa end of the femur
+  femurToTibiaVector = np.array([[0]*5]*6)  #Vector from terminus of the previous femur vector to the pivot point for the femur end of the tibia
+  tibiaToEndVector = np.array([[0]*5]*6)    #Vector from terminus of the previous tibia vector to the point of the end effector
+
+  def __init__(self, robotFile=None):
     #slurp deets from file
 
+    if robotFile:
+      f = open(robotFile, 'r')
+      for line in f:
+        line = line.strip()
+        print len(line)
+        if (len(line) is 0) | (line[0] is '#'): 
+          print "here"
+          continue
+        arr = np.array([x.strip() for x in line.split([',','|'])])
+        #arr.shape = (2,3)
+        print arr 
+
+  
+      
+  
 
